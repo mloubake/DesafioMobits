1160f34fae97b0d1852771b168098a90ef73c6f1
mloubake <mloubake@hotmail.com>
2019-08-01 03:22:20 -0300

    Limpando código.

--- app/src/main/java/br/com/mloubake/desafiomobits/activity/SaqueActivity.java
+++ app/src/main/java/br/com/mloubake/desafiomobits/activity/SaqueActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,6 +67,7 @@ public class SaqueActivity extends AppCompatActivity {
     }
 
     public void sacarValor() {
+
         valorSacado= 0f;
         if(!TextUtils.isEmpty(etSaque.getText())) {
             valorSacado = Float.parseFloat(etSaque.getText().toString());
